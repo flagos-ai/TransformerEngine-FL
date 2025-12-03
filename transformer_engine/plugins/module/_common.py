c2b35a23ab8e89403c0a2c7109a80532aa5bed4c
lxd-cumt <1141051934@qq.com>
2025-12-03 15:04:36 +0800

    polish

--- transformer_engine/plugins/module/_common.py
+++ transformer_engine/plugins/module/_common.py
@@ -9,7 +9,7 @@ from typing import Any, Callable, Dict, List, Optional, Tuple, Union
 from ..import_utils import safety_import
 
 ### RMSNORM
-gems_rmsnorm_fwd = safety_import('transformer_engine.plugins.cpp_extensions.gems_rms_norm', 'rms_norm_forward')
+fl_rmsnorm_fwd = safety_import('transformer_engine.plugins.cpp_extensions', 'fl_rmsnorm_fwd')
 
 def fl_apply_normalization(
     inputmat: torch.Tensor,
@@ -23,7 +23,7 @@ def fl_apply_normalization(
     fwd_ln_sm_margin: int,
     zero_centered_gamma: bool,
 ):
-    normalization_func = gems_rmsnorm_fwd
+    normalization_func = fl_rmsnorm_fwd
     ln_out, rsigma = normalization_func(
         inputmat,
         [inputmat.shape[-1]],
