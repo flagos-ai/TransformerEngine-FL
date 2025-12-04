98bc42f003ceaf4dca0c47a2b637d48186fbf91d
Xianduo Li <30922914+lxd-cumt@users.noreply.github.com>
2025-12-04 15:38:28 +0800

    Add the FlagOS multi-chip backend for TransformerEngine (#1)
    # Description
    
    Refactor TransformerEngine for FlagOS
    
    ## Type of change
    
    - [ ] Documentation change (change only to the documentation, either a
    fix or a new content)
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [x] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] Infra/Build change
    - [ ] Code refactoring
    
    ## Changes
    
    Please list the changes introduced in this PR:
    
    - gemm
    - linear / layernorm_linear
    - attention
    - rms_norm
    - adamw
    
    # Checklist:
    
    - [ ] I have read and followed the [contributing
    guidelines](https://github.com/NVIDIA/TransformerEngine/blob/main/CONTRIBUTING.rst)
    - [ ] The functionality is complete
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit tests pass locally with my changes
    
    ---------
    
    Co-authored-by: zhaoyinglia <ylzhao@baai.ac.cn>

---
+++ transformer_engine/plugins/cpp_extensions/rmsnorm.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2025, BAAI. All rights reserved.
+#
+# See LICENSE for license information.
+
+import os
+import torch
+from typing import Any, Callable, Dict, List, Optional, Tuple, Union
+
+from ..import_utils import safety_import, have_flag_gems
+
+### RMSNORM
+HAVE_FLAG_GEMS = have_flag_gems()
+
+if HAVE_FLAG_GEMS:
+    rmsnorm_fwd = safety_import('transformer_engine.plugins.cpp_extensions.rms_norm_fl', 'rms_norm_forward')
+    rmsnorm_bwd = safety_import('transformer_engine.plugins.cpp_extensions.rms_norm_fl', 'rms_norm_backward')
+else:
+    rmsnorm_fwd = None
+    rmsnorm_bwd = None
+
+def rmsnorm_fwd_fl(
+    input,
+    weight,
+    eps,
+    ln_out,
+    quantizer,
+    odtype,
+    sm_margin,
+    zero_centered_gamma,
+):
+    assert HAVE_FLAG_GEMS, "GEMS is not installed"
+    y, rstdevs = rmsnorm_fwd(
+        input,
+        [input.shape[-1]],
+        weight,
+        eps,
+    )
+    return y, None, rstdevs
+
+
+def rmsnorm_bwd_fl(
+    dy,
+    x,
+    rsigma,
+    gamma,
+    sm_margin,
+    zero_centered_gamma,
+    eps,
+):
+    assert HAVE_FLAG_GEMS, "GEMS is not installed"
+    dx, dw = rmsnorm_bwd(
+        dy,
+        x,
+        rsigma,
+        [x.shape[-1]],
+        gamma,
+        eps,
+    )
+    return dx, dw
